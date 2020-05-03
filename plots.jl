082a38c8c2676ea1c38e29108c041638899b441f
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-03 19:54:05 +0200

    Plot format fix.

--- plots.jl
+++ plots.jl
@@ -8,8 +8,8 @@ using Plots; pyplot()
 wig_csv_path = "https://stooq.pl/q/d/l/?s=wig&d1=20200101&d2=20200430&i=d"
 sp500_csv_path = "https://stooq.pl/q/d/l/?s=^spx&d1=20200101&d2=20200501&i=d"
 
-download(wig_csv_path, "data/wig.csv")
-download(sp500_csv_path, "data/sp500.csv")
+# download(wig_csv_path, "data/wig.csv")
+# download(sp500_csv_path, "data/sp500.csv")
 
 df_wig = CSV.read("data/wig.csv")[:, [:Data, :Zamkniecie, :Wolumen]]
 df_sp500 = CSV.read("data/sp500.csv")[:, [:Data, :Zamkniecie, :Wolumen]]
@@ -20,5 +20,8 @@ rename!(df_sp500, [:Zamkniecie => :sp500, :Wolumen => :volume_sp500])
 df_all = join(df_wig, df_sp500, on = :Data, kind = :inner)
 rename!(df_all, :Data => :date)
 
-plot(df_all.sp500, draw_arrow = true)
-plot!(twinx(), df_all.wig, c = :red)
+plt = plot()
+plot!(plt, df_all.date, df_all.sp500, label = "sp500")
+plot!(twinx(), df_all.date, df_all.wig, c = :red, label = "wig", legend = :bottomright)
+
+savefig("data/wig_sp500.png")
