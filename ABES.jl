18ee75f7729f6c904a4e38bf8e1ed21e026d5d18
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-18 11:18:54 +0200

    Finished simulate function. No tests done yet.

--- ABES.jl
+++ ABES.jl
@@ -36,13 +36,16 @@ mutable struct AgentModel{T<:AbstractFloat}
     carrier_time::Distribution
 end
 
-function simulate(m::AgentModel{T}, max_iter::Int64 = 20, seed::Int64 = 1234) where T<:AbstractFloat
+function simulate(m::AgentModel{T}, max_iter::Int64 = 20, c_count₀::Int64 = 1, i_count₀::Int64 = 0) where T<:AbstractFloat
     if max_iter <= 0 throw(DomainError(max_iter, "argument must be greater than 0")) end
 
     population = fill(Agent(suspectible, UInt16(0)), nv(m.G))
-    population[rand(1:nv(m.G))] = Agent(carrier, UInt16(1))
 
-    state = DataFrame(s=nv(m.G)-1, e=0, i=0, c=1, d=0, r=0)
+    view(population, rand(1:nv(m.G), c_count₀)) .= [ Agent(carrier, round(UInt16, rand(m.carrier_time))) for i in 1:c_count₀ ]
+    view(population, rand(1:nv(m.G), i_count₀)) .= [ Agent(infected, round(UInt16, rand(m.infected_time))) for i in 1:i_count₀ ]
+
+    s_count, e_count, i_count, c_count, d_count, r_count = Threads.Atomic{Int64}{nv(m.G)-c_count₀-i_count₀}, Threads.Atomic{Int64}{0}, Threads.Atomic{Int64}{i_count₀}, Threads.Atomic{Int64}{c_count₀}, Threads.Atomic{Int64}{0}, Threads.Atomic{Int64}{0}
+    state = DataFrame("suspectible"=nv(m.G)-1, "exposed"=e_count[], "infected"=i_count[], "carrier"=c_count[], "dead"=d_count[], "recovered"=r_count[])
 
     iteration = 1
     while iteration <= max_iter
@@ -67,17 +70,34 @@ function simulate(m::AgentModel{T}, max_iter::Int64 = 20, seed::Int64 = 1234) wh
         @inbounds Threads.@threads for i in eachindex(population)
             if population[i].end_time > 0 && population[i].end_time == iteration
                 if population[i].condition == exposed
+                    Threads.atomic_sub!(e_count, UInt16(1))
                     if rand() < m.γ
                         population[i] = Agent(infected, round(UInt16, iteration + rand(m.infected_time)))
+                        Threads.atomic_add!(i_count, UInt16(1))
                     else
                         population[i] = Agent(carrier, round(UInt16, iteration + rand(m.carrier_time)))
+                        Threads.atomic_add!(c_count, UInt16(1))
                     end
                 elseif population[i].condition == infected
-                    population[i].condition = rand() < m.δ ? dead : recovered
+                    Threads.atomic_sub!(i_count, UInt16(1))
                     population[i].end_time = UInt16(0)
+                    if rand() < m.δ
+                        population[i].condition = dead
+                        Threads.atomic_add!(d_count, UInt16(1))
+                    else
+                        population[i].condition = recovered
+                        Threads.atomic_add!(r_count, UInt16(1))
+                    end
                 elseif population[i].condition == carrier
-                    population[i].condition = rand() < m.ζ ? suspectible : recovered
+                    Threads.atomic_sub!(c_count, UInt16(1))
                     population[i].end_time = UInt16(0)
+                    if rand() < m.ζ
+                        population[i].condition = suspectible
+                        Threads.atomic_add!(s_count, UInt16(1))
+                    else
+                        population[i].condition = recovered
+                        Threads.atomic_add!(r_count, UInt16(1))
+                    end
                 end
             end
         end
@@ -88,10 +108,16 @@ function simulate(m::AgentModel{T}, max_iter::Int64 = 20, seed::Int64 = 1234) wh
             end
         end
 
-        return population
+        new_infections_numb = sum(new_infections)
+        s_count[] -= new_infections_numb
+        e_count[] += new_infections_numb
+
+        push!(state, s_count[], e_count[], i_count[], c_count[], d_count[], r_count[])
 
         iteration += 1
     end
+
+    return state
 
 end
 
